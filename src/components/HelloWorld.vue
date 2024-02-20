385deb9a0b02010795f6ed7cd70e5f432fb143f8
Kutimo <marius-oliver@hotmail.com>
2024-02-20 20:48:25 +0100

    removed unused components and added padding and margin,

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,8 +17,23 @@ import ButtonSecondary from './buttons/ButtonSecondary.vue'
     <p class="body-lg">body lg Lorem ipsum dolor sit amet.</p>
     <p class="body-md">body md Lorem ipsum dolor sit amet.</p>
   </div>
-  <ButtonPrimary button-text="Buy now" />
-  <ButtonSecondary button-text="secondary" />
+  <div class="buttons">
+    <ButtonPrimary button-text="Buy now" />
+    <ButtonSecondary button-text="secondary" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.greetings {
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  gap: 15px;
+}
+
+.buttons {
+  display: flex;
+  margin: 10px;
+  gap: 15px;
+}
+</style>
