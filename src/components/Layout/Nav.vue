a809091f51710a5abbd26cce99b2be0c87a0a96d
Kutimo2 <marius-oliver@hotmail.com>
2024-02-21 16:26:19 +0100

    removed unused icons and added new icons

--- src/components/Layout/Nav.vue
+++ src/components/Layout/Nav.vue
@@ -14,18 +14,23 @@ export default {
         <a href="https://digital.avonova.com/index">Digital HR og HMS</a>
       </li>
       <li>
-        <a href="https://www.avonova.no/kurs"></a>
+        <a href="https://www.avonova.no/kurs">Kurs</a>
         <img src="../icons/IconArrow.svg" alt="Kurs icon" height="10" width="15" />
       </li>
       <li>
-        <a href="https://www.avonova.no/ansattegoder/helseforsikring"></a>
+        <a href="https://www.avonova.no/ansattegoder/helseforsikring">Helseforsikring</a>
         <img src="../icons/IconArrow.svg" alt="Helseforsikring icon" height="10" width="15" />
       </li>
     </ul>
+    <div>
+      <button>
+        <img src="../icons/IconUser.svg" alt="Brukermeny" height="40" width="40" />
+      </button>
+      <button>
+        <img src="../icons/IconQuestion.svg" alt="hjelpemeny" height="40" width="40" />
+      </button>
+    </div>
   </nav>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
