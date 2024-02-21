6e52c62dd95fe13a279a2764f47b064af94f6db3
Kutimo2 <marius-oliver@hotmail.com>
2024-02-21 17:42:57 +0100

    added focus styles, gray color to variables and changed layout

--- src/components/Layout/Nav.vue
+++ src/components/Layout/Nav.vue
@@ -6,22 +6,24 @@ export default {
 
 <template>
   <nav>
-    <a href="https://digital.avonova.com/services">
-      <img src="@/assets/logo.svg" alt="Avonova logo" height="21" width="143" />
-    </a>
-    <ul>
-      <li>
-        <a href="https://digital.avonova.com/index">Digital HR og HMS</a>
-      </li>
-      <li>
-        <a href="https://www.avonova.no/kurs">Kurs</a>
-        <img src="../icons/IconArrow.svg" alt="Kurs icon" height="10" width="15" />
-      </li>
-      <li>
-        <a href="https://www.avonova.no/ansattegoder/helseforsikring">Helseforsikring</a>
-        <img src="../icons/IconArrow.svg" alt="Helseforsikring icon" height="10" width="15" />
-      </li>
-    </ul>
+    <div class="nav-links">
+      <a href="https://digital.avonova.com/services">
+        <img src="@/assets/logo.svg" alt="Avonova logo" height="21" width="143" />
+      </a>
+      <ul>
+        <li>
+          <a href="https://digital.avonova.com/index">Digital HR og HMS</a>
+        </li>
+        <li>
+          <a href="https://www.avonova.no/kurs">Kurs</a>
+          <img src="../icons/IconArrow.svg" alt="Kurs icon" height="10" width="15" />
+        </li>
+        <li>
+          <a href="https://www.avonova.no/ansattegoder/helseforsikring">Helseforsikring</a>
+          <img src="../icons/IconArrow.svg" alt="Helseforsikring icon" height="10" width="15" />
+        </li>
+      </ul>
+    </div>
     <div class="buttons">
       <button>
         <img src="../icons/IconUser.svg" alt="Brukermeny" height="18" width="20" />
@@ -34,6 +36,25 @@ export default {
 </template>
 
 <style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 16px 18px;
+}
+.nav-links {
+  display: flex;
+}
+
+.nav-links > a {
+  margin-right: 64px;
+}
+
+ul {
+  display: flex;
+  gap: 32px;
+}
 li {
   list-style: none;
   display: flex;
@@ -58,6 +79,7 @@ a:hover {
 }
 
 button {
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -66,5 +88,23 @@ button {
   border-radius: 100px;
   height: 40px;
   width: 40px;
+}
+
+button:hover {
+  background-color: var(--gray300);
+  cursor: pointer;
+}
+
+button:focus:before {
+  content: ' ';
+  position: absolute;
+  top: -6px;
+  left: -6px;
+  right: -6px;
+  bottom: -6px;
+  border: 1.5px solid #01332a;
+  border-radius: 50%;
+  opacity: 1;
+  background-color: transparent;
 }
 </style>
