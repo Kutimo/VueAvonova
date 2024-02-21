9d7640b2d55e84ba6327a713c31e8ecdcda317a1
Kutimo2 <marius-oliver@hotmail.com>
2024-02-21 12:31:11 +0100

    added icon and error text to component

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-
 export default defineComponent({
   name: 'Input',
   props: {
@@ -12,11 +11,23 @@ export default defineComponent({
       type: String,
       required: true
     },
+    placeholder: {
+      type: String,
+      default: ''
+    },
     descriptiveText: {
       type: String,
       default: ''
     },
     icon: {
+      type: String,
+      default: ''
+    },
+    error: {
+      type: Boolean,
+      default: true
+    },
+    errorText: {
       type: String,
       default: ''
     }
@@ -29,14 +40,19 @@ export default defineComponent({
     <label for="{{name}}">
       {{ label }}
       <p>{{ descriptiveText }}</p>
-      <input id="{{name}}" type="{{text }}" />
+      <input id="{{name}}" type="text" :placeholder="placeholder" />
     </label>
+    <div v-if="error" class="input__error">
+      <img class="input__error-img" src="../icons/IconError.svg" alt="error icon" />
+      <p class="input__error-text">{{ errorText }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .input {
   display: flex;
+  flex-direction: column;
   gap: 10px;
 }
 input {
@@ -51,5 +67,17 @@ input:hover,
 input:active {
   border: 2px solid;
   border-color: var(--green1100);
+}
+
+.input__error {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.input__error-text {
+  color: var(--red);
+  font-size: 14px;
+  height: 18px;
 }
 </style>
