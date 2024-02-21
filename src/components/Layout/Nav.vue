822877f1165cded59d787dd37bf2777919457c47
Kutimo2 <marius-oliver@hotmail.com>
2024-02-21 18:28:55 +0100

    started work with mobile menu

--- src/components/Layout/Nav.vue
+++ src/components/Layout/Nav.vue
@@ -7,28 +7,31 @@ export default {
 <template>
   <nav>
     <div class="nav-links">
+      <button class="nav-menu">
+        <img src="../icons/IconOpenMenu.svg" alt="Meny" height="16" width="12" />
+      </button>
       <a href="https://digital.avonova.com/services">
         <img src="@/assets/logo.svg" alt="Avonova logo" height="21" width="143" />
       </a>
       <ul>
-        <li>
+        <li class="nav-link">
           <a href="https://digital.avonova.com/index">Digital HR og HMS</a>
         </li>
-        <li>
+        <li class="nav-link">
           <a href="https://www.avonova.no/kurs">Kurs</a>
           <img src="../icons/IconArrow.svg" alt="Kurs icon" height="10" width="15" />
         </li>
-        <li>
+        <li class="nav-link">
           <a href="https://www.avonova.no/ansattegoder/helseforsikring">Helseforsikring</a>
           <img src="../icons/IconArrow.svg" alt="Helseforsikring icon" height="10" width="15" />
         </li>
       </ul>
     </div>
-    <div class="buttons">
-      <button>
+    <div class="buttons-right">
+      <button class="button-right">
         <img src="../icons/IconUser.svg" alt="Brukermeny" height="18" width="20" />
       </button>
-      <button>
+      <button class="button-right">
         <img src="../icons/IconQuestion.svg" alt="hjelpemeny" height="40" width="40" />
       </button>
     </div>
@@ -43,12 +46,24 @@ nav {
   width: 100%;
   padding: 16px 18px;
 }
+
+.nav-menu {
+  margin-right: 40px;
+  display: none;
+  width: 56px;
+  height: 32px;
+  z-index: 15;
+}
+
 .nav-links {
   display: flex;
+  align-items: center;
 }
 
 .nav-links > a {
   margin-right: 64px;
+  display: flex;
+  align-items: center;
 }
 
 ul {
@@ -73,12 +88,24 @@ a:hover {
   text-decoration: underline;
 }
 
-.buttons {
+.buttons-right {
   display: flex;
   gap: 10px;
 }
 
-button {
+@media only screen and (max-width: 960px) {
+  .nav-link {
+    display: none;
+  }
+  .buttons-right {
+    display: none;
+  }
+  .nav-menu {
+    display: block;
+  }
+}
+
+.button-right {
   position: relative;
   display: flex;
   justify-content: center;
@@ -90,12 +117,12 @@ button {
   width: 40px;
 }
 
-button:hover {
+.button-right:hover {
   background-color: var(--gray300);
   cursor: pointer;
 }
 
-button:focus:before {
+.button-right:focus:before {
   content: ' ';
   position: absolute;
   top: -6px;
