34b282b89b9d993341ab9680c41e2e9e69576139
Kutimo <marius-oliver@hotmail.com>
2024-02-26 10:41:59 +0100

    change css to tailwind in product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,15 +1,3 @@
-<template>
-  <div class="productCard">
-    <img v-if="cardIcon" :src="cardIcon" class="cardIcon" alt="Card Icon" />
-    <h4 v-if="cardHeader" class="cardHeader">{{ cardHeader }}</h4>
-    <p class="body-md, infoSection" v-html="cardContent"></p>
-    <div class="buttonSection">
-      <ButtonPrimary buttonText="Les mer" @click="onReadMore" />
-      <ButtonPrimary buttonText="Bestill" @click="onBookAppointment" />
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 import ButtonPrimary from "@/components/buttons/ButtonPrimary.vue";
 
@@ -28,39 +16,25 @@ const onBookAppointment = () => {
 };
 </script>
 
-<style scoped>
-.productCard {
-  display: flex;
-  flex-direction: column;
-  background-color: #e0fff9 /* var(--green-600) */;
-  border-radius: var(--radius-4);
-  overflow: hidden;
-  height: 20%;
-  max-width: 35%;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  margin: var(--spacing-10);
-}
-.cardIcon {
-  width: 30%;
-  margin: 0 auto;
-  padding: var(--spacing-4);
-}
-.cardHeader {
-  text-align: center;
-  padding: var(--spacing-4);
-}
-.iconImgSection {
-  text-align: center;
-  padding: var(--spacing-4);
-}
-.infoSection {
-  padding: 20px;
-  text-align: center;
-}
+<template>
+  <div
+    class="rounded-4 m-10 flex w-[250px] flex-col items-center overflow-hidden bg-green-600 p-4 shadow-md"
+  >
+    <img
+      v-if="cardIcon"
+      :src="cardIcon"
+      class="p-4"
+      alt="Card Icon"
+      height="75"
+      width="75"
+    />
+    <h4 v-if="cardHeader" class="p-4 text-center">{{ cardHeader }}</h4>
+    <p class="body-md px-20 text-center" v-html="cardContent"></p>
+    <div class="flex w-full justify-around p-4">
+      <ButtonPrimary buttonText="Les mer" @click="onReadMore" />
+      <ButtonPrimary buttonText="Bestill" @click="onBookAppointment" />
+    </div>
+  </div>
+</template>
 
-.buttonSection {
-  display: flex;
-  justify-content: space-around;
-  padding: var(--spacing-4);
-}
-</style>
+<style scoped></style>
