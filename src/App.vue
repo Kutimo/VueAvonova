8d36bdae48d54eb4836b1ce0581278866403b083
Kutimo <marius-oliver@hotmail.com>
2024-02-28 08:45:54 +0100

    moved product cards to home view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import Navigation from "@/components/Layout/Navigation.vue";
 import Footer from "./components/Layout/Footer.vue";
 import ProductCard from "@/components/ProductCard.vue";
@@ -8,34 +7,11 @@ import HelloWorld from "./components/HelloWorld.vue";
 //Måtte legge productCardIcons i public mappen for at det skulle funke
 //Se på måte for og importere ikonene fra assets mappen (@/assets/productCardIcons)
 //Burde denne legges i en egen mappe, så man bygger kortet og eksporterer det til App.vue?
-const cards = ref([
-  {
-    id: 1,
-    cardIcon: "/public/productCardIcons/hospital.svg",
-    cardHeader: "Helsesjekk",
-    cardContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 2,
-    cardIcon: "/public/productCardIcons/notepad.svg",
-    cardHeader: "Helseattest",
-    cardContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-]);
 </script>
 
 <template>
   <Navigation />
   <router-view></router-view>
-  <main>
-    <ProductCard
-      v-for="card in cards"
-      :key="card.id"
-      :cardIcon="card.cardIcon"
-      :cardHeader="card.cardHeader"
-      :cardContent="card.cardContent"
-    />
-  </main>
   <section>
     <HelloWorld />
   </section>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,43 @@
 <script lang="ts">
-export default {
+import { defineComponent, ref } from "vue";
+import ProductCard from "@/components/ProductCard.vue";
+
+export default defineComponent({
   name: "Home",
-};
+  components: {
+    ProductCard,
+  },
+  setup() {
+    const cards = ref([
+      {
+        id: 1,
+        cardIcon: "/public/productCardIcons/hospital.svg",
+        cardHeader: "Helsesjekk",
+        cardContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+      {
+        id: 2,
+        cardIcon: "/public/productCardIcons/notepad.svg",
+        cardHeader: "Helseattest",
+        cardContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+    ]);
+
+    return { cards };
+  },
+});
 </script>
 
 <template>
-  <div id="Home">
-    <h1>Home</h1>
-  </div>
+  <main>
+    <ProductCard
+      v-for="card in cards"
+      :key="card.id"
+      :cardIcon="card.cardIcon"
+      :cardHeader="card.cardHeader"
+      :cardContent="card.cardContent"
+    />
+  </main>
 </template>
 
 <style scoped></style>
