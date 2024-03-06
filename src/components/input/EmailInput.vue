60a84d2bc556ace74209245215e2436d364196de
Kutimo <marius-oliver@hotmail.com>
2024-03-07 00:43:59 +0100

    add view, route for login and email component

---
+++ src/components/input/EmailInput.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+export default {
+  name: "EmailInput",
+  data() {
+    return {
+      email: "",
+      emailError: "",
+    };
+  },
+  methods: {
+    sanitizeEmail() {
+      const regex =
+        /^[-!#-'*+\/-9=?^-~]+(?:\.[-!#-'*+\/-9=?^-~]+)*@[-!#-'*+\/-9=?^-~]+(?:\.[-!#-'*+\/-9=?^-~]+)+$/i; // Regex to match a basic email format
+      if (!regex.test(this.email)) {
+        this.emailError = "Skriv inn en gyldig epost adresse";
+      } else {
+        this.emailError = "";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="flex h-[50px] max-w-[360px] flex-col gap-8">
+    <label for="email" class="font-body text-xl font-normal"> Epost </label>
+    <input
+      class="active::border-green-1100 h-40 w-[260px] rounded-8 border-[1px] border-gray-600 px-10 py-8 text-base hover:border-2 hover:border-green-1100 focus-visible:border-2 focus-visible:outline-none active:border-2"
+      id="email"
+      type="email"
+      v-model.trim="email"
+      @blur="sanitizeEmail"
+    />
+    <p v-if="emailError" class="text-red">{{ emailError }}</p>
+  </div>
+</template>
---
+++ src/views/ViewLogin.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import EmailInput from "@/components/input/EmailInput.vue";
+export default {
+  name: "ViewLogin",
+  components: {
+    EmailInput,
+  },
+};
+</script>
+
+<template>
+  <main class="h-screen">
+    <EmailInput />
+  </main>
+</template>
