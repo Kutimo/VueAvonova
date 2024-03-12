e923e5c53972f6d8e55568341082f1fd8a7bad11
Kutimo <marius-oliver@hotmail.com>
2024-03-12 03:15:02 +0100

    add password input

---
+++ src/components/input/PasswordInput.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+  name: "PasswordInput",
+};
+</script>
+
+<template>
+  <div class="flex h-[50px] max-w-[360px] flex-col gap-8">
+    <label for="password" class="font-body text-xl font-normal">
+      Passord
+    </label>
+    <input
+      class="active::border-green-1100 h-40 w-[260px] rounded-8 border-[1px] border-gray-600 px-10 py-8 text-base hover:border-2 hover:border-green-1100 focus-visible:border-2 focus-visible:outline-none active:border-2"
+      id="password"
+      type="password"
+    />
+  </div>
+</template>
--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,15 +1,18 @@
 <script lang="ts">
 import EmailInput from "@/components/input/EmailInput.vue";
+import PasswordInput from "@/components/input/PasswordInput.vue";
 export default {
   name: "ViewLogin",
   components: {
     EmailInput,
+    PasswordInput,
   },
 };
 </script>
 
 <template>
-  <main class="h-screen">
+  <main class="flex h-screen gap-10">
     <EmailInput />
+    <PasswordInput />
   </main>
 </template>
