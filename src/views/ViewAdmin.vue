23d48dc4ac719c2c6a0be8749d48dc54787d8b11
Kutimo <marius-oliver@hotmail.com>
2024-05-06 12:54:36 +0200

    Add admin components to easily change user metadata

---
+++ src/views/ViewAdmin.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { supabase } from "@/lib/supabaseClient";
+
+type User = {
+  id: string;
+  email: string;
+  user_metadata: {
+    // Define the properties of user_metadata here
+    // For example:
+    name: string;
+    age: number;
+  };
+};
+
+export default {
+  name: "ViewAdmin",
+  components: {},
+
+  setup() {
+    const sendMetaData = async (): Promise<void> => {
+      const {
+        data: { user },
+      } = (await supabase.auth.getUser()) as { data: { user: User | null } };
+      if (user) {
+        let metadata = user.user_metadata;
+        // Use metadata here
+      } else {
+        // TODO: Add toast
+      }
+    };
+    return {
+      sendMetaData,
+    };
+  },
+};
+</script>
+
+<template>
+  <main></main>
+</template>
