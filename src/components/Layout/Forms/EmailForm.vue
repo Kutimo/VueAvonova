f727f4a0f858b81feb0d49525022b3642fe7dbd2
wroan92 <anders-wroldsen@live.com>
2024-05-06 11:58:39 +0200

    Add SMTPJS dependency and update script tag in index.html. And started Email form and smtp.js

---
+++ src/components/Layout/Forms/EmailForm.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <h2>Contact Form</h2>
+        <form @submit.prevent="sendEmail">
+            <div>
+                <label for="name">Navn:</label>
+                <input type="text" id="name" v-model="name" required>
+            </div>
+            <div>
+                <label for="email">Epost:</label>
+                <input type="email" id="email" v-model="email" required>
+            </div>
+            <div>
+                <label for="phone">Tlf Nummer:</label>
+                <input type="tel" id="phone" v-model="phone" required>
+            </div>
+            <div>
+                <label for="message">Din melding:</label>
+                <textarea id="message" v-model="message" required></textarea>
+            </div>
+            <button type="submit">Send Epost</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface EmailFormData {
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
+}
+
+export default defineComponent({
+    data(): EmailFormData {
+        return {
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        };
+    },
+    methods: {
+        sendEmail() {
+            (window as any).Email.send({
+                SecureToken: "43790b23-27f6-4cde-abd5-4cdf2b2c75e7",
+                To: 'anders-wroldsen@live.com',
+                From: this.email,
+                Subject: "Epost fra Avonova assist bedrifthelsetjenester",
+                Body: `Name: ${this.name}<br>Email: ${this.email}<br>Phone: ${this.phone}<br>Message: ${this.message}`
+            }).then((message: string) => alert(message));
+
+            this.name = '';
+            this.email = '';
+            this.phone = '';
+            this.message = '';
+        }
+    }
+});
+</script>
+
+
