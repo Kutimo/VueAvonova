3aa539ac35c4324a286ddf9cd412ba32fa49fa3c
rubideveloper <rubengrytten@gmail.com>
2024-05-07 11:41:15 +0200

    Corrected with

--- src/components/select/Select.vue
+++ src/components/select/Select.vue
@@ -1,7 +1,7 @@
 <template>
   <select
     v-model="selectedValue"
-    :class="`active::border-green-1100 h-40 w-[160px] rounded-6 border-[1px] border-gray-600 px-10 py-8 hover:border-2 hover:border-green-1100 focus-visible:border-2 focus-visible:outline-none active:border-2 ${extraClasses}`"
+    :class="`active::border-green-1100 h-40 w-[260px] rounded-6 border-[1px] border-gray-600 px-10 py-8 hover:border-2 hover:border-green-1100 focus-visible:border-2 focus-visible:outline-none active:border-2 ${extraClasses}`"
   >
     <option v-for="option in options" :key="option.value" :value="option.value">
       {{ option.label }}
