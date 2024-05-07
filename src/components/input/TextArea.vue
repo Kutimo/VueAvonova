0be9e5f3b91eee391822f1f721df00840cc46b15
rubideveloper <rubengrytten@gmail.com>
2024-05-07 11:37:56 +0200

    Added with and height to textarea

--- src/components/input/TextArea.vue
+++ src/components/input/TextArea.vue
@@ -2,7 +2,7 @@
   <textarea
     v-model="value"
     :placeholder="placeholder"
-    :class="`rounded-md p-2 w-full resize-none border ${extraClasses}`"
+    :class="`active::border-green-1100 h-[170px] w-[359px] rounded-6 border-[1px] border-gray-600 px-10 py-8 hover:border-2 hover:border-green-1100 focus-visible:border-2 focus-visible:outline-none active:border-2 ${extraClasses}`"
   ></textarea>
 </template>
 
