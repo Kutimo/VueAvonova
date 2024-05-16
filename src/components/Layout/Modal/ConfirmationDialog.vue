f59afd044a5fbe9714eb272631561c081d5ca71e
rubideveloper <rubengrytten@gmail.com>
2024-05-16 11:18:32 +0200

    Changed header and styling

--- src/components/Layout/Modal/ConfirmationDialog.vue
+++ src/components/Layout/Modal/ConfirmationDialog.vue
@@ -13,16 +13,16 @@ const onBookAppointment = () => {
 
 <template>
   <div
-    class="m-10 flex h-[375px] w-[500px] justify-center overflow-hidden rounded-10 px-20 shadow-md"
+    class="flex h-[300px] w-[500px] justify-center overflow-hidden rounded-10 px-20 shadow-md"
   >
     <div class="flex items-center justify-center w-full">
       <div>
-        <h4>Er informasjonen du har lagt til korrekt?</h4>
+        <h4 class="flex justify-center">Korrekt informasjon?</h4>
         <p class="mt-20 text-base">
           Er du sikker på at informasjonen du har lagt til er korrekt? Ved og klikke
           "Fullfør bestilling", bekrefter du samtidig at samtlige opplysninger stemmer.
         </p>
-        <div class="flex justify-center w-full mt-40 gap-30">
+        <div class="flex justify-center w-full mt-48 gap-30">
           <ButtonSecondary buttonText="Tilbake" @click="onBookAppointment" />
           <ButtonPrimary buttonText="Fullfør bestilling" @click="onReadMore" />
         </div>
